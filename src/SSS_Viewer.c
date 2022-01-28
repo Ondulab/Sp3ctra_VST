7459a134d18aa08c3ffe2102e35388e05ac61adb
patrick.reybaud <patrick.reybaud@electro-crea.com>
2022-01-28 01:10:05 +0100

    Add UDP split packets feature

--- src/SSS_Viewer.c
+++ src/SSS_Viewer.c
@@ -21,16 +21,21 @@
 #include <termios.h>
 #include <unistd.h>
 
-#define UDP_HEADER								(1397969715) //01010011 01010011 01010011 00110011 SSS3
-#define UDP_HEADER_SIZE							(1)//uint32
-#define CIS_ACTIVE_PIXELS_PER_LINE				(1152)
-#define CIS_LINE_LEN 							(CIS_ACTIVE_PIXELS_PER_LINE * 3)
+//#define CALIBRATION
 
-#define BUFLEN 									((CIS_LINE_LEN + UDP_HEADER_SIZE) * 2)	//Max length of buffer
+#define CIS_PIXELS_PER_LINE						(1152 / 2)//200DPI
+#define CIS_LEN 								(CIS_PIXELS_PER_LINE * 3)
+
+#define UDP_HEADER								(1397969715) 	//01010011 01010011 01010011 00110011 SSS3
+#define UDP_HEADER_SIZE							(4)//uint8
+#define UDP_NB_PACKET_PER_LINE					(7)
+#define UDP_PACKET_SIZE							((((CIS_LEN) + (UDP_HEADER_SIZE)) / UDP_NB_PACKET_PER_LINE) * 4)
+
+#define BUFLEN 									((CIS_LEN + UDP_HEADER_SIZE) * 2 * 4)	//Max length of buffer
 #define PORT 									(55151)	//The port on which to listen for incoming data
 
-#define WINDOWS_WIDTH							(CIS_LINE_LEN)
-#define WINDOWS_HEIGHT							(2160)
+#define WINDOWS_WIDTH							(CIS_LEN)
+#define WINDOWS_HEIGHT							(1160)
 
 
 //memo on linux terminal : sudo nc -u -l 55151
@@ -49,7 +54,8 @@ int main(void)
 	int y = 0;
 	int recv_len = 0;
 	unsigned int slen = sizeof(si_other);
-	uint32_t buf[BUFLEN];
+	uint8_t buf[BUFLEN];
+	static uint32_t curr_packet = 0;
 
 	//--------------------------------------SDL2 INIT-------------------------------------------//
 	SDL_Window *window = NULL;
@@ -57,21 +63,6 @@ int main(void)
 	SDL_Texture* background_texture;
 	SDL_Texture* foreground_texture;
 	int statut = EXIT_FAILURE;
-
-	/* Rectangles for drawing which will specify source (inside the texture)
-	  and target (on the screen) for rendering our textures. */
-	SDL_Rect SrcR;
-	SDL_Rect DestR;
-
-	SrcR.x = 0;
-	SrcR.y = 0;
-	SrcR.w = WINDOWS_WIDTH;
-	SrcR.h = WINDOWS_HEIGHT - 1;
-
-	DestR.x = 0;
-	DestR.y = 1;
-	DestR.w = WINDOWS_WIDTH;
-	DestR.h = WINDOWS_HEIGHT - 1;
 
 	/* Initialisation, création de la fenêtre et du renderer. */
 	if(0 != SDL_Init(SDL_INIT_VIDEO))
@@ -136,37 +127,64 @@ int main(void)
 		//		printf("Waiting for data...");
 		fflush(stdout);
 
-		//try to receive some data, this is a blocking call
-		if ((recv_len = recvfrom(s, buf, BUFLEN * 2, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+		for (curr_packet = 0; curr_packet < (UDP_NB_PACKET_PER_LINE * 2); curr_packet++)
 		{
-			die("recvfrom()");
+			//try to receive some data, this is a blocking call
+			if ((recv_len = recvfrom(s, &buf[curr_packet * UDP_PACKET_SIZE], UDP_PACKET_SIZE, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+			{
+				die("recvfrom()");
+			}
 		}
 
 		//print details of the client/peer and the data received
 		//		printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
 		//		printf("Data: %s\n" ,(uint8_t *)buf);
 
-		int max = 0;
 		int offset = 0;
 
-		for (int i = 0; i < (BUFLEN / 2); i++)
+		for (int i = 0; i < (BUFLEN / 2); i+=4)
 		{
-			if(buf[i] > max)
+			if (buf[i] == 0b00110011)
 			{
-				max = buf[i];
-				offset = i;
+				if (buf[i + 1] == 0b01010011)
+				{
+					if (buf[i + 2] == 0b01010011)
+					{
+						if (buf[i + 3] == 0b01010011)
+						{
+							offset = i + UDP_HEADER_SIZE;
+						}
+
+					}
+				}
 			}
 		}
 
-		offset += UDP_HEADER_SIZE;
 
-		//				y++;
 
-		for (int x = 0; x < CIS_LINE_LEN; x++)
+
+#ifdef CALIBRATION
+		uint32_t red = 0, green = 0, blue = 0;
+#endif
+		for (int x = 0; x < CIS_LEN; x++)
 		{
-			SDL_SetRenderDrawColor(renderer, buf[offset + x] / 256, buf[offset+ x] / 256, buf[offset + x] / 256, 255);
+			SDL_SetRenderDrawColor(renderer, buf[offset + (x * 4)], buf[offset + (x * 4) + 1], buf[offset + (x * 4) + 2], 255);
 			SDL_RenderDrawPoint(renderer, WINDOWS_WIDTH - x, y % WINDOWS_HEIGHT);
+#ifdef CALIBRATION
+			red += buf[offset + (x * 4)];
+			green += buf[offset + (x * 4) + 1];
+			blue += buf[offset + (x * 4) + 2];
+#endif
 		}
+#ifdef CALIBRATION
+		//for calibrate CIS leds luminosity
+		red /= CIS_LEN;
+		green /= CIS_LEN;
+		blue /= CIS_LEN;
+		printf("RED : %d   GREEN : %d   BLUE : %d    \n" , red, green, blue);
+		SDL_Delay(100);
+#endif
+
 
 		SDL_SetRenderTarget(renderer, foreground_texture);// Dorénavent, on modifie à nouveau le renderer
 
@@ -189,6 +207,7 @@ int main(void)
 		SDL_RenderCopy(renderer, foreground_texture, NULL, &position);
 
 		SDL_RenderPresent(renderer);
+//		SDL_Delay(1);
 
 		SDL_SetRenderTarget(renderer, background_texture);
 
