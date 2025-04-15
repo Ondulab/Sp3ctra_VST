fc548569aa4b78ac028926106ddc41a15e508c52
Zhonx <patrick-reybaud@users.noreply.github.com>
2025-04-15 21:44:54 +0200

    Add nogui version

--- build_qmake/launch_cisynth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script de lancement pour CISYNTH
-APP_PATH="/Users/zhonx/Documents/Workspaces/Workspace_Xcode/CISYNTH/build_qmake/CISYNTH.app"
-
-echo "Lancement de CISYNTH..."
-open "${APP_PATH}"
