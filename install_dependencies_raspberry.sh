efaa4ca8ec9aa1cd6b942c13ee43e28ccaa78b2d
Zhonx <patrick-reybaud@users.noreply.github.com>
2025-05-24 01:15:53 +0200

    fix: add librtmidi-dev dependency

--- install_dependencies_raspberry.sh
+++ install_dependencies_raspberry.sh
@@ -12,7 +12,7 @@ sudo apt update
 echo "Installation des bibliothèques de développement..."
 sudo apt install -y build-essential libqt5gui5 libqt5core5a qtbase5-dev qttools5-dev-tools qt5-qmake qtchooser
 sudo apt install -y libfftw3-dev libsndfile1-dev
-sudo apt install -y libasound2-dev librtaudio-dev
+sudo apt install -y libasound2-dev librtaudio-dev librtmidi-dev
 sudo apt install -y libpthread-stubs0-dev
 
 # Optionnel: SFML (si disponible)
