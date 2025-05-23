50478471fdb0107d58d3eae69056a7d2af8d27f8
Zhonx <patrick-reybaud@users.noreply.github.com>
2025-05-24 01:11:46 +0200

    fix: add qt5-qmake and qtchooser for qmake

--- install_dependencies_raspberry.sh
+++ install_dependencies_raspberry.sh
@@ -10,7 +10,7 @@ sudo apt update
 
 # Installer les dépendances requises
 echo "Installation des bibliothèques de développement..."
-sudo apt install -y build-essential qt5-default libqt5gui5 libqt5core5a qtbase5-dev-tools
+sudo apt install -y build-essential qt5-default libqt5gui5 libqt5core5a qtbase5-dev-tools qt5-qmake qtchooser
 sudo apt install -y libfftw3-dev libsndfile1-dev
 sudo apt install -y libasound2-dev librtaudio-dev
 sudo apt install -y libpthread-stubs0-dev
